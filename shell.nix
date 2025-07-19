dac0d6a53980939aedb190fe9b694d997ba6731d
Ruby Iris Juric <ruby@srxl.me>
2025-07-19 15:44:09 +1000

    nix: replace flake with plain devshell
    Don't really need the flake for anything anymore. Keep the shell simple.
    
    Signed-off-by: Ruby Iris Juric <ruby@srxl.me>

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/fa64ec5c1ca6f17746f3defedb988b9248e97616.tar.gz";
+  }) { };
+in
+
+pkgs.mkShell {
+  name = "developer.rebble.io-devshell";
+  packages = with pkgs; [
+    bundler
+    nodejs
+    ruby
+    python3
+    zlib
+  ];
+}
