5cdd234acd59934da49b2028d7e4dc61be967beb
Ruby Iris Juric <ruby@srxl.me>
2025-08-04 14:08:32 +1000

    plugins/redirects: add redirect for old /developer.pebble.com/ subpaths
    Signed-off-by: Ruby Iris Juric <ruby@srxl.me>

--- plugins/generator_redirects.rb
+++ plugins/generator_redirects.rb
@@ -13,22 +13,34 @@
 # limitations under the License.
 
 module Jekyll
-
   class GeneratorRedirects < Generator
-
     priority :lowest
 
-    def initialize(config)
-    end
+    def initialize(config) end
 
     def generate(site)
       @site = site
       site.data['redirects'].each do |redirect|
-        if is_infinite_redirect?(redirect[0], redirect[1])
-          Jekyll.logger.warn "Redirect Warning:", "Skipping redirect of #{redirect[0]} to #{redirect[1]}"
+        if infinite_redirect?(redirect[0], redirect[1])
+          Jekyll.logger.warn 'Redirect Warning:', "Skipping redirect of #{redirect[0]} to #{redirect[1]}"
           next
         end
-        @site.pages << RedirectPage.new(@site, @site.source, File.dirname(redirect[0]), File.basename(redirect[0]), redirect[1])
+        @site.pages <<
+          RedirectPage.new(@site, @site.source, File.dirname(redirect[0]), File.basename(redirect[0]), redirect[1])
+      end
+
+      # Generate redirects for the old URLs under the /developer.pebble.com/ subpath to new, subpath-less location
+      site.pages.each do |page|
+        next if page.url.start_with?('/developer.pebble.com/')
+        next if page.url.start_with?('/assets/')
+
+        if page.url.end_with?('/')
+          @site.pages <<
+            RedirectPage.new(@site, @site.source, "/developer.pebble.com#{page.url}", 'index.html', page.url)
+        else
+          @site.pages <<
+            RedirectPage.new(@site, @site.source, File.dirname("/developer.pebble.com#{page.url}"), File.basename(page.url), page.url)
+        end
       end
     end
 
@@ -36,27 +48,25 @@ module Jekyll
 
     # Returns true if the redirect pair (from, to) will cause an infinite
     # redirect.
-    def is_infinite_redirect?(from, to)
+    def infinite_redirect?(from, to)
       return true if from == to
-      return true if File.basename(from) == 'index.html' && File.dirname(from) == File.dirname(to + 'index.html')
+
+      return true if File.basename(from) == 'index.html' && File.dirname(from) == File.dirname("#{to}index.html")
+
       false
     end
-
   end
 
   class RedirectPage < Page
-
     def initialize(site, base, dir, name, redirect_to)
       @site = site
       @base = base
       @dir = dir
       @name = name.empty? ? 'index.html' : name
 
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts', 'utils'), 'redirect_permanent.html')
-      self.data['redirect_to'] = redirect_to
+      process(@name)
+      read_yaml(File.join(base, '_layouts', 'utils'), 'redirect_permanent.html')
+      data['redirect_to'] = redirect_to
     end
-
   end
-
 end
